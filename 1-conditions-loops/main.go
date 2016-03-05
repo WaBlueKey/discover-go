d00f2c9f0dc9b02298eb664c15bcf6b89dc4ac43
Mackenzie  Adams <Zee@Mackenzies-iMac.local>
2016-03-05 17:00:34 -0500

    task 1 conditional loops completed

---
+++ 1-conditions-loops/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+
+	//Generate a psuedo-random number between 0 - 100.
+	randomNumber := rand.Intn(100)
+
+	//if statement to see if number is < or > 50.
+	if randomNumber > 50 {
+		fmt.Println("my random number is", randomNumber, "and is greater than 50")
+	} else {
+		fmt.Println("my random number is", randomNumber, "and is less than 50")
+	}
+	//Assign Holberton School string to a variable.
+	school := "Holberton School"
+
+	if school == "Holberton School" {
+		fmt.Println("I am a student of the", school)
+	}
+
+	//Assign boolean value of true to beautifulWeather.
+	beautifulWeather := true
+
+	if beautifulWeather == true {
+		fmt.Println("It's a beautiful weather!")
+	}
+
+	//Assign a variable with the three founders' names.
+	holbertonFounders := []string{"Rudy Rigot", "Sylvain Kalache", "Julien Barbier"}
+
+	for _, each := range holbertonFounders {
+		fmt.Printf("%s is a founder\n", each)
+	}
+}
