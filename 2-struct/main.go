4f5ca0053d895c2b7c7723416fcccbf1bbebb4a0
Zee Adams <wabluekey@gmail.com>
2016-03-07 15:53:46 -0500

    refined steps

--- 2-struct/main.go
+++ 2-struct/main.go
@@ -12,19 +12,18 @@ type user struct {
 }
 
 func age(u user) {
-	value := "March 7, 1917"           //actual DOB in string.
-	form := "January 2, 2006"          //form setting standard date to identify DOB.
-	year, _ := time.Parse(form, value) //making DOB readable as a date and not string.
-	birthyear := year.Year()           //extracting birth year from DOB.
-	today := time.Now()                //setting today's date.
-	Yeartoday := today.Year()          //extracting current year from today's date.
-	age := Yeartoday - birthyear       //subtracting years to find out age.
+	form := "January 2, 2006"             //form setting standard date to identify DOB.
+	DOB, err := time.Parse(form, u.DOB)   //making DOB readable as a date and not string.
+	age := time.Now().Year() - DOB.Year() //subtracting years to find out age.
 
-	fmt.Printf("%s who was born in %s would be %d years old today.\n", u.Name, u.City, age)
+	if err != nil {
+		fmt.Println("Please try again. Error:", err)
+	} else {
+		fmt.Printf("%s who was born in %s would be %d years old today.\n", u.Name, u.City, age)
+	}
 }
 func main() {
 	u := user{Name: "Betty Holberton", DOB: "March 7, 1917", City: "Philadelphia"}
 	fmt.Printf("Hello %s\n", u.Name)
-
 	age(u)
 }
